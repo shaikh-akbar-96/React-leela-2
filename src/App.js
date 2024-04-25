07adfb293531a93d4ea8d72ed61c2404c01dbda5
shaikh-akbar-96 <akbarhshaikh1996@gmail.com>
2024-04-25 18:03:47 +0530

    learnt about useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import Counter from "./components/Counter/Counter";
 
 function App() {
+  const [showCounter, setShowCounter] = useState(true);
+
+  function toggleHandler() {
+    setShowCounter(!showCounter);
+  }
   return (
     <div className="container mx-auto">
-      <Counter />
+      <div>
+        <button
+          className="bg-pink-500 text-white px-3 py-1 my-2"
+          onClick={toggleHandler}
+        >
+          Toggle Component
+        </button>
+      </div>
+      <div>{showCounter && <Counter />}</div>
     </div>
   );
 }
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,29 +1,77 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Counter() {
+  const isMounted = useRef(false);
   const [counter, setCounter] = useState(0);
-  function Increment() {
+  const [x, setX] = useState(0);
+
+  function increment() {
     setCounter(counter + 1);
   }
-  function Decrement() {
+  function decrement() {
     setCounter(counter - 1);
   }
+
+  //when componentDidMount and Update
+  useEffect(() => {
+    console.log("Fires on ComponentDidMount and Update");
+  });
+
+  //when componentDidMount only
+  useEffect(() => {
+    console.log("Fires on ComponentDidMount");
+  }, [counter]);
+
+  //when component unmounted and also update
+
+  useEffect(() => {
+    return () => {
+      console.log("Fires on componentWillUnmount and update");
+    };
+  });
+
+  // when component unmounted only
+
+  useEffect(() => {
+    return () => {
+      console.log("Fires on componentWillUnmount");
+    };
+  }, []);
+
+  // when component Update only
+
+  useEffect(() => {
+    if (isMounted.current) {
+      console.log("Fires ComponentDidUpdate");
+    } else {
+      isMounted.current = true;
+    }
+  });
+
   return (
     <div>
       <div className="">Counter Component</div>
       <div className="">Counter: {counter}</div>
+      <div className="">X: {x}</div>
       <div>
         <button
           className="bg-green-500 my-2 px-2 py-1 text-white"
-          onClick={Increment}
+          onClick={increment}
         >
           Increment
         </button>
         <button
           className="bg-red-500 my-2 mx-2 px-2 py-1 text-white"
-          onClick={Decrement}
+          onClick={decrement}
         >
           Decrement
+        </button>
+
+        <button
+          className="bg-green-500 my-2 px-2 py-1 text-white"
+          onClick={() => setX(x + 1)}
+        >
+          Increment X
         </button>
       </div>
     </div>
